69199ba24ec3297cc1613b42e5de766fc35b52e9
parasgirdhar <parasy219@gmail.com>
2026-09-24 06:47:08 +0530

    Time: 10 ms (97.05%), Space: 48.3 MB (50.28%) - LeetHub

---
+++ 0382-linked-list-random-node/0382-linked-list-random-node.java
@@ -0,0 +1,24 @@
+
+class Solution {
+
+    ArrayList <Integer> al = new ArrayList<>();
+    
+    public Solution(ListNode head) {
+
+    
+
+      while( head != null){
+        al.add( head.val);
+        head = head.next;
+      }
+
+    }
+    
+    public int getRandom() {
+          int p = (int)(Math.random()*al.size());
+          return al.get(p);
+    }
+     
+}
+
+
